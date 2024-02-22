bd7824acf527c4a4a24e483eb9d5aa5bd7c0210b
Disda-coding <497457669@qq.com>
2024-02-22 16:32:56 +0800

    修改了全局异常处理类，使其不会暴露后端代码问题

---
+++ ledger/src/main/java/com/ruoyi/ledger/service/ILedgerDevDetailsService.java
@@ -0,0 +1,64 @@
+package com.ruoyi.ledger.service;
+
+import com.ruoyi.ledger.domain.LedgerDevDetails;
+
+import java.util.List;
+
+/**
+ * 设备详情Service接口
+ * 
+ * @author disda
+ * @date 2024-02-22
+ */
+public interface ILedgerDevDetailsService 
+{
+    /**
+     * 查询设备详情
+     * 
+     * @param id 设备详情主键
+     * @return 设备详情
+     */
+    public LedgerDevDetails selectLedgerDevDetailsById(Long id);
+
+    /**
+     * 查询设备详情列表
+     * 
+     * @param ledgerDevDetails 设备详情
+     * @return 设备详情集合
+     */
+    public List<LedgerDevDetails> selectLedgerDevDetailsList(LedgerDevDetails ledgerDevDetails);
+
+    /**
+     * 新增设备详情
+     * 
+     * @param ledgerDevDetails 设备详情
+     * @return 结果
+     */
+    public int insertLedgerDevDetails(LedgerDevDetails ledgerDevDetails);
+
+    /**
+     * 修改设备详情
+     * 
+     * @param ledgerDevDetails 设备详情
+     * @return 结果
+     */
+    public int updateLedgerDevDetails(LedgerDevDetails ledgerDevDetails);
+
+    /**
+     * 批量删除设备详情
+     * 
+     * @param ids 需要删除的设备详情主键集合
+     * @return 结果
+     */
+    public int deleteLedgerDevDetailsByIds(Long[] ids);
+
+    /**
+     * 删除设备详情信息
+     * 
+     * @param id 设备详情主键
+     * @return 结果
+     */
+    public int deleteLedgerDevDetailsById(Long id);
+
+    List<String> getDevTypeOps();
+}
